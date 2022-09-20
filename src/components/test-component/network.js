73167638d5f2e318d19e8687fc559e3a7f84ebf8
Diego33KX <diegofj203@gmail.com>
2022-09-19 22:59:47 -0500

    feat: add implement register and login function

--- src/components/test-component/network.js
+++ src/components/test-component/network.js
@@ -5,5 +5,6 @@ const testRouter = Router();
 
 testRouter.route("/").get(Controller.findAll);
 testRouter.route("/").post(Controller.create);
-
+testRouter.route("/login").post(Controller.login);
+testRouter.route("/hola").get(Controller.ensureToken, Controller.hola)
 export default testRouter;
