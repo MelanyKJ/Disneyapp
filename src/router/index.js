1268486c356fa01ab51231fc52f673818d1d364b
Diego33KX <diegofj203@gmail.com>
2022-09-20 22:41:12 -0500

    feat: add character create and login

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { TestRouter } from "../components";
+import { RouterMovie } from "../components";
+import { RouterCharacter } from "../components";
 import { RouterGender } from "../components";
 import { ensureToken } from "../components/test-component/controller";
 // cada vez que quiera agregar unaruta nueva,
 // creo el path e importo el componente
-const listRoutes = [["/auth", TestRouter],["/gender", RouterGender]];
+
+
+const listRoutes = [["/auth", TestRouter],["/movie",RouterMovie],["/character",RouterCharacter],["/gender", RouterGender]];
 
 export const routes = (app) => {
   listRoutes.forEach(([path, controller]) => {
